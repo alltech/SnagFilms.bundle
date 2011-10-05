4b7bd6df6e8fa9dfe0db7af330c97abf22902076
Ian Bird <IanDBird@Gmail.com>
2011-10-05 17:55:09 +0100

    added extra metadata

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -2,6 +2,7 @@ import re
 
 VIDEO_PREFIX = "/video/snagfilms"
 
+TITLE = "SnagFilms"
 BASE_URL = "http://www.snagfilms.com"
 
 BASE_BROWSE = "http://www.snagfilms.com/films/browse/"
@@ -14,7 +15,7 @@ ICON = 'icon-default.png'
 
 ####################################################################################################
 def Start():
-  Plugin.AddPrefixHandler(VIDEO_PREFIX, MainMenu, "SnagFilms", ICON, ART)
+  Plugin.AddPrefixHandler(VIDEO_PREFIX, MainMenu, TITLE, ICON, ART)
   Plugin.AddViewGroup("InfoList", viewMode = "InfoList", mediaType = "items")
   Plugin.AddViewGroup("List", viewMode = "List", mediaType = "items")
 
@@ -34,7 +35,7 @@ def Start():
 
 ####################################################################################################
 def MainMenu():
-    oc = ObjectContainer(view_group='List')
+    oc = ObjectContainer()
     
     oc.add(DirectoryObject(key = Callback(ListItems, title = "Most Liked", url = MOST_LIKED), title = "Most Liked"))
     oc.add(DirectoryObject(key = Callback(ListItems, title = "New Arrivals", url = NEW_ARIVALS), title = "New Arrivals"))
@@ -48,7 +49,7 @@ def MainMenu():
 ####################################################################################################
 
 def Categories(title):
-    oc = ObjectContainer(view_group='List', title2 = title)
+    oc = ObjectContainer(title2 = title)
 
     page = HTML.ElementFromURL(BASE_BROWSE)
     for category in page.xpath("//ul[@class = 'categories']/li")[1:-1]:
@@ -62,7 +63,7 @@ def Categories(title):
 ####################################################################################################
 
 def Channels(title):
-    oc = ObjectContainer(view_group='List', title2 = title)
+    oc = ObjectContainer(title2 = title)
 
     page = HTML.ElementFromURL(BASE_BROWSE)
     for channel in page.xpath("//li[@class='channels']//li/a"):
@@ -76,16 +77,19 @@ def Channels(title):
 ####################################################################################################
 
 def AllFilms(title):
-    oc = ObjectContainer(view_group='List', title2 = title)
+    oc = ObjectContainer(title2 = title)
+    
     for letter in list(string.uppercase):
         oc.add(DirectoryObject(key = Callback(ListItems, title = letter, url = AZ_URL % letter), title = letter))
+    
     return oc
 
 ####################################################################################################
 
-def ListItems(title, url):
-    oc = ObjectContainer(title2 = title, view_group = 'InfoList')
+def ListItems(title, url, replace_parent = False):
+    oc = ObjectContainer(view_group = 'InfoList', title2 = title, replace_parent = replace_parent)
 
+    # Add all the items currently found on the page.
     page = HTML.ElementFromURL(url)
     for item in page.xpath("//ul[contains(@class, 'films-list')]/li"):
         title = item.xpath(".//h3[@class='title']//text()")[0].strip()
@@ -93,18 +97,39 @@ def ListItems(title, url):
         thumb = item.xpath(".//img")[0].get('src')
         summary = item.xpath(".//div[@class='summary']/p/text()")[0].strip()
 
+        date = None
+        duration = None
+        extra = ''.join(item.xpath(".//div[@class='f-footer']/p/text()")).strip('\n').strip()
+        try:
+            extra_dict = re.match(".*\((?P<date>[0-9]+)\) (?P<mins>[0-9]+)  mins.*", extra).groupdict()
+            date = Datetime.ParseDate(extra_dict['date'])
+            duration = int(extra_dict['mins'])
+            duration = duration * 60 * 1000
+        except: pass
+        
+        rating_desc = item.xpath(".//span[contains(@class, 'stars')]")[0].get('class')
+        rating_dict = re.match(".*s(?P<rating>[0-5]+)", rating_desc).groupdict()
+        rating = float(rating_dict['rating']) * 2
+        
         oc.add(VideoClipObject(
             url = pageUrl,
             title = title,
             summary = summary,
-            thumb = Function(GetThumb, url = thumb)
+            thumb = Function(GetThumb, url = thumb),
+            duration = duration,
+            originally_available_at = date,
+            rating = rating
         ))
 
+    # If we find another page, add it so that the user can further navigate.
     next_page = page.xpath("//div[@class = 'pagination']//li[@class = 'next']")
     if len(next_page) > 0:
         next_url = next_page[0].xpath(".//a")[0].get('href')
-        oc.add(DirectoryObject(key = Callback(ListItems, title = title, url = BASE_URL + next_url), title = "Next..."))
+        oc.add(DirectoryObject(key = Callback(ListItems, title = title, url = BASE_URL + next_url, replace_parent = True), title = "Next..."))
     
+    if len(oc) == 0:
+        return MessageContainer(title, "No titles were found...")   
+
     return oc
 
 ####################################################################################################
